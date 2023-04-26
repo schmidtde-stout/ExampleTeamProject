abda8400df7f5798f5245f879f4899175466f7df
schmidtde-stout <schmidtde@uwstout.edu>
2023-04-26 09:28:32 -0500

    added a hello label

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,18 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			Label hello = new Label("Hello");
+			BorderPane root = new BorderPane(hello);
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
